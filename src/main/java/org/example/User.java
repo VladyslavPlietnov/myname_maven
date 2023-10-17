3a8c09868b57d4baee24150f61426f679cab21c4
Plietnov Vladyslav <pletniov@ukr.net>
2023-10-17 09:50:30 +0200

    second commit

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -1,2 +1,10 @@
-package org.example;public class User {
+package org.example;
+
+public class User {
+    String name;
+    String lastName;
+    public User(String firstName, String lastName){
+        name = firstName;
+        this.lastName = lastName;
+    }
 }
